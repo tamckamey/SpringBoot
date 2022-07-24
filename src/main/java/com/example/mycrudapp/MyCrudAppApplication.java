0c685cb5dadab22d98803a854ce885b574ae0353
Tanner McKamey <tannermckamey@outlook.com>
2022-07-24 19:57:51 -0400

    Adding Spring files.

---
+++ src/main/java/com/example/mycrudapp/MyCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mycrudapp/MyCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
